1d516de9f5a2211b56dc196784a9fcc2499283a3
Mathieu <107419157+matjcqm@users.noreply.github.com>
2022-10-18 14:19:44 +0200

    commit

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,102 @@
 // récupération du tableau du canapés dans l'API
 
+<<<<<<< HEAD
+import { fetchData } from "./utils/fetch.js";
+
+// récupération du lien de l'API
+
+const urlcanapes = "http://localhost:3000/api/products";
+
+// récupération du tableau du LocalStorage
+
+let cartTableau = JSON.parse(localStorage.getItem("products"));
+
+// Affichage des produits qui se trouvent dans le LocalStorage
+
+const displayCart = async () => {
+  const tableauCanapes = await fetchData(urlcanapes); //Tableau des canapés de l'API
+  const carte = document.querySelector("#cart__items");
+  if (cartTableau) {
+    cartTableau.map((item) => {
+      const canape = tableauCanapes.find(
+        (idCanap) => idCanap._id == item.idProduct
+      );
+      const article = document.createElement("article");
+      article.classList.add("cart__item");
+      article.setAttribute("data-id", item.idProduct);
+      article.setAttribute("data-color", item.color);
+      carte.appendChild(article);
+      const divImage = document.createElement("div");
+      divImage.classList.add("cart__item__img");
+      article.appendChild(divImage);
+      const image = document.createElement("img");
+      image.setAttribute("src", canape.imageUrl);
+      image.setAttribute("alt", canape.altTxt);
+      divImage.appendChild(image);
+      const divContent = document.createElement("div");
+      divContent.classList.add("cart__item__content");
+      article.appendChild(divContent);
+      const divDescription = document.createElement("div");
+      divDescription.classList.add("cart__item__content__description");
+      divContent.appendChild(divDescription);
+      const name = document.createElement("h2");
+      name.innerHTML = canape.name;
+      divDescription.appendChild(name);
+      const color = document.createElement("p");
+      color.innerHTML = item.color;
+      divDescription.appendChild(color);
+      const price = document.createElement("p");
+      price.innerHTML = `${canape.price},00€`;
+      divDescription.appendChild(price);
+      const divSettings = document.createElement("div");
+      divSettings.classList.add("cart__item__content__settings");
+      divContent.appendChild(divSettings);
+      const divQuantity = document.createElement("div");
+      divQuantity.classList.add("cart__item__content__settings__quantity");
+      divSettings.appendChild(divQuantity);
+      const quantity = document.createElement("p");
+      quantity.innerHTML = `Qté : ${item.quantity}`;
+      divQuantity.appendChild(quantity);
+      const inputQuantity = document.createElement("input");
+      inputQuantity.classList.add("itemQuantity");
+      inputQuantity.setAttribute("type", Number);
+      inputQuantity.setAttribute("name", "itemQuantity");
+      inputQuantity.setAttribute("min", 1);
+      inputQuantity.setAttribute("max", 100);
+      inputQuantity.setAttribute("value", item.quantity);
+      divQuantity.appendChild(inputQuantity);
+      const divDelete = document.createElement("div");
+      divDelete.classList.add("cart__item__content__settings__delete");
+      divSettings.appendChild(divDelete);
+      const buttonDelete = document.createElement("p");
+      buttonDelete.classList.add("deleteItem");
+      buttonDelete.innerHTML = "Supprimer";
+      divDelete.appendChild(buttonDelete);
+    });
+  } else {
+    const text = document.createElement("h2");
+    text.innerHTML = "Votre panier est vide";
+    carte.appendChild(text);
+  }
+};
+
+displayCart();
+
+const totalQuantity = document.querySelector("#totalQuantity");
+totalQuantity.innerHTML = cartTableau.length; // Calcul du nombre de produit dans le panier (prendre encore en compte la quantité)
+
+
+const totalPrice = document.querySelector("#totalPrice");
+// totalPrice.innerHTML = item.price
+
+const order = document.querySelector("#order");
+order.addEventListener("click", function () {
+  if (cartTableau) {
+  } else {
+    window.alert("Votre panier est vide");
+  }
+});
+=======
 import { fetchData } from "./utils/fetch.js"
 
 
@@ -25,3 +122,4 @@ const displayCart = async () => {
 //     })
 // }
 // displayProduct()
+>>>>>>> 9254583c32a8dca340f20ad20aa992833a90f566
--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,72 @@
 // récupération du tableau du canapés dans l'API
 
+<<<<<<< HEAD
+import { fetchData } from "./utils/fetch.js";
+
+// récupération de l'ID du produit dans l'URL
+
+const params = window.location.search;
+const idProduct = new URLSearchParams(params).get("id");
+
+// récupération du lien de l'API lié au produit
+
+const urlProduct = `http://localhost:3000/api/products/${idProduct}`;
+
+// Création et affichage des données du produit dans le DOM
+
+const displayProduct = async () => {
+  const product = await fetchData(urlProduct);
+  document.querySelector('title').innerHTML = `${product.name}`;
+  document.querySelector(
+    ".item__img"
+  ).innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}" />`;
+  document.querySelector("#title").innerHTML = `${product.name}`;
+  document.querySelector("#price").innerHTML = `${product.price}`;
+  document.querySelector("#description").innerHTML = `${product.description}`;
+  const colorList = document.querySelector("#colors");
+
+  // Boucle de création d'une balise 'option' par couleur sur le produit
+
+  product.colors.map((color) => {
+    const optionColor = document.createElement("option");
+    optionColor.innerHTML = color;
+    optionColor.setAttribute("value", color);
+    colorList.appendChild(optionColor);
+  });
+};
+displayProduct();
+
+// Fonction du bouton ajouter au panier
+
+const addCart = document.querySelector("#addToCart");
+addCart.addEventListener("click", (e) => {
+  //Réaction au click sur le bonton
+  e.preventDefault();
+  const color = document.querySelector("#colors").value;
+  const quantity = parseInt(document.querySelector("#quantity").value);
+  const productOptions = {
+    idProduct,
+    quantity,
+    color,
+  };
+  let cartItems = JSON.parse(localStorage.getItem("products")) || []; // On récupère le tableau du LocalStorage ou alors création d'un tableau vide
+  let cartItem = cartItems.find(
+    (item) => item.idProduct == idProduct && item.color == color
+  ); // Fonction find pour vérifier si l'ID et la couleur sont similaires
+  if (!color || quantity <= 0) {
+    // Affichage d'un message d'erreur si il n'y a pas de couleur ou de quantité sélectionnée
+    window.alert("Veuillez choisir une couleur et une quantité");
+  } else if (cartItem) {
+    // Si la fonction find is true, aditionner les quantités
+    cartItem.quantity = parseInt(cartItem.quantity) + quantity;
+  } else {
+    // Sinon ajouter le produit dans le tableau du LocalStorage
+    cartItems.push(productOptions);
+  }
+  localStorage.setItem("products", JSON.stringify(cartItems)); // Envoie du tableau dans le LocalStorage
+  window.alert("Votre produit a été ajouté au panier"); // Message de validation du panier
+});
+=======
 import { fetchData } from "./utils/fetch.js"
 
 // récupération de l'ID du produit dans l'URL
@@ -50,3 +117,4 @@ addCart.addEventListener('click', function()  {
     }
     //envoi du localStorage avec les infos du produits
 })
+>>>>>>> 9254583c32a8dca340f20ad20aa992833a90f566
